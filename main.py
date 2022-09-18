e6f4b961cef696d7eabb391087eece11a536a86f
r!oxm <53299920+Imad00x@users.noreply.github.com>
2022-09-18 16:58:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-# main.py
-from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from fastapi import FastAPI, Request
+from requests import get
+from urllib.request import urlopen
 import requests
+from bs4 import BeautifulSoup
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 app = FastAPI()
 
-machine_ip = requests.get('http://ipecho.net/plain?').text
 
-@app.get("/",response_class=PlainTextResponse)
-def hello():
-    return f'ip: {machine_ip}'
+@app.get("/", response_class=PlainTextResponse)
+async def main():
+    url = "https://ziaprojet.000webhostapp.com/http.txt"
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
 
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    text = text.split('\n')
+    return text
